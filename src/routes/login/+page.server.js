f8f148948493dfa28c139d844d3c710b9311b4bc
sparrowsl <benthorpe.dev@gmail.com>
2024-01-24 08:07:56 +0000

    feat: add login route with action

---
+++ src/routes/login/+page.server.js
@@ -0,0 +1,35 @@
+import db from "$lib/server/db.js";
+import { usersTable } from "$lib/server/schema.js";
+import { redirect } from "@sveltejs/kit";
+import { eq } from "drizzle-orm";
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	default: async ({ request, cookies }) => {
+		const form = Object.fromEntries(await request.formData());
+
+		// TODO: validate the form input
+
+		// Check if username exists in database
+		const isValid = await db.query.usersTable.findFirst({
+			where: eq(usersTable.username, String(form.username)),
+		});
+
+		if (!isValid) {
+			return {
+				errors: { message: "Invalid Username and Password!!" },
+			};
+		}
+		// TODO: check hashed password using bcrypt
+
+		// Set session cookies for the user
+		const { password, updated, created, ...user } = isValid;
+		cookies.set("session", JSON.stringify(user), {
+			path: "/",
+			httpOnly: true,
+			maxAge: 24 * 24 * 60 * 7,
+		});
+
+		redirect(307, "/rooms");
+	},
+};
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { enhance } from "$app/forms";
+	import FormInput from "$lib/components/FormInput.svelte";
+</script>
+
+<section>
+	<form action="" method="post" use:enhance>
+		<fieldset class="grid gap-5">
+			<FormInput name="username" label="Username" />
+			<FormInput type="password" name="password" label="Password" />
+			<button class="w-fit">Login</button>
+		</fieldset>
+	</form>
+</section>
