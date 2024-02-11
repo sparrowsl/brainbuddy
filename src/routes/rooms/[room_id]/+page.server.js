a3110d21aea0c3e6035fe66abdc3ebde300bc5c8
sparrowsl <benthorpe.dev@gmail.com>
2024-02-11 23:04:06 +0000

    feat: load room data and comments

--- src/routes/rooms/[room_id]/+page.server.js
+++ src/routes/rooms/[room_id]/+page.server.js
@@ -1,15 +1,46 @@
 import db from "$lib/server/db.js";
-import { roomsTable } from "$lib/server/schema.js";
-import { eq } from "drizzle-orm";
+import { messagesTable, roomsTable } from "$lib/server/schema.js";
+import { desc, eq } from "drizzle-orm";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
+	// biome-ignore lint/nursery/useAwait: <explanation>
+	const getRoomComments = async () => {
+		return db.query.messagesTable.findMany({
+			where: eq(messagesTable.roomId, params.room_id),
+			orderBy: desc(messagesTable.created),
+			with: {
+				user: {
+					columns: {
+						id: true,
+						username: true,
+					},
+				},
+			},
+		});
+	};
+
 	const room = await db.query.roomsTable.findFirst({
 		where: eq(roomsTable.id, params.room_id),
 		with: {
-			messages: true,
+			host: {
+				columns: {
+					id: true,
+					username: true,
+					email: true,
+				},
+			},
+			topic: {
+				columns: {
+					id: true,
+					name: true,
+				},
+			},
 		},
 	});
 
-	return { room };
+	return {
+		room,
+		comments: getRoomComments(),
+	};
 }
--- src/routes/rooms/[room_id]/+page.svelte
+++ src/routes/rooms/[room_id]/+page.svelte
@@ -2,10 +2,30 @@
 	export let data;
 </script>
 
-<pre>
-{JSON.stringify(data?.room, null, 2)}
-</pre>
-
 <h1>Room: {data.room?.name}</h1>
-<p><a href="/rooms">back to rooms</a></p>
-<!-- <h2></h2> -->
+<h2>
+	Hosted by <span class="text-blue-600">@{data.room?.host?.username}</span>
+</h2>
+<p class="whitespace-pre-wrap">{data.room?.description}</p>
+<p>{data.room?.topic?.name}</p>
+<hr />
+
+<h2 class="font-semibold uppercase text-gray-700">Conversation</h2>
+
+{#await data.comments}
+	<p>loading comments...</p>
+{:then comments}
+	{#each comments as comment (comment.id)}
+		<figure class="">
+			<h3>
+				<span>@{comment.user?.username}</span>
+				<span class="text-gray-700 text-sm italic">{comment.created}</span>
+			</h3>
+			<figcaption>
+				<p>{comment.body}</p>
+			</figcaption>
+		</figure>
+	{:else}
+		<p>no comments yet</p>
+	{/each}
+{/await}
